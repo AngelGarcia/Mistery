17dd78b4f021575ca6359658794ab1f48ff432fb
Ángel García Logrosán <aglogrosan@gmail.com>
2025-09-24 13:57:26 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: 'studio-7956312296-90b9d',
+  appId: '1:28472885242:web:040ebde3ece9f341daae43',
+  apiKey: 'AIzaSyBf2ONh7iZGK5xLMrcx_9fZLgU8ETBG3TI',
+  authDomain: 'studio-7956312296-90b9d.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '28472885242',
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
